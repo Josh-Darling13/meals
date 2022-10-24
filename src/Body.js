9cdaf6c469336613efcc458c05856d8ad45ccfab
Josh Darling <71955043+Josh-Darling13@users.noreply.github.com>
2022-10-23 21:18:34 -0400

    successful get of API data

--- src/Body.js
+++ src/Body.js
@@ -1,11 +1,25 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import axios from "axios";
+
 // import bootstrap from ""
 
 function Body(){
+
+    useEffect(() => {
+
+        axios.get(`https://themealdb.com/api/json/v1/1/filter.php?c=Seafood`).then(res=>{
+            console.log(res.data);
+        }).catch(err=>{
+            console.log(err);
+        })
+
+    }, [])
+
+    // idMeal strMeal strMealThumb
+
     return(
         <h1>
-            Body
+            Body Shots
         </h1>
     )
 
