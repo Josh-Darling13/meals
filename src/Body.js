81341447f50b5eebba5ee275c769dce979d57812
Josh Darling <71955043+Josh-Darling13@users.noreply.github.com>
2022-10-25 03:02:24 -0400

    added font awesome icons in react updated style

--- src/Body.js
+++ src/Body.js
@@ -1,54 +1,43 @@
 import React, {useEffect, useState} from "react";
 import axios from "axios";
-
 import bootstrap from 'bootstrap/dist/css/bootstrap.min.css'
-import { Button } from "bootstrap";
 
 function Body(){
 
     const [items, setitems]=useState([]);
 
     useEffect(() => {
-
         axios.get(`https://themealdb.com/api/json/v1/1/filter.php?c=Seafood`).then(res=>{
-            // If the code runs
             console.log(res.data);
-
             setitems(res.data.meals)
-
         }).catch(err=>{
-            // if it doesn't log an error message
             console.log(err);
-        }) 
+        })
     }, [])
-
-    // idMeal strMeal strMealThumb
-
 
     const itemList = items.map((obj)=>{
         const mealID = obj.idMeal;
         const mealName = obj.strMeal.replace(/\s+/g, '-');
         console.log(mealName)
         const url = `https://www.themealdb.com/meal/${mealID}-${mealName}`;
-        // console.log(url);
+
         return(
             <div className="col-md-4">
                 <a href={url} target='blank' >
-                <p key={obj.idMeal}>{obj.strMeal}</p>
-                <img src={obj.strMealThumb} alt={obj.strMeal} className='img-fluid'/>
+                    <img src={obj.strMealThumb} alt={obj.strMeal} className='float-end'/>
+                </a>
+                <a href={url} target='blank' >
+                    <p className='food-name' >{obj.strMeal}</p>
                 </a>
             </div>
         )
     });
 
-
     return(
         <div>
-            <div className="row">
-            {itemList}
-
+            <div className="row justify-content-center">
+                {itemList}
             </div>
-
         </div>
     )
 }
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,4 +1,10 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faEnvelope} from '@fortawesome/free-solid-svg-icons'
+
+
+
+const emailIcon = <FontAwesomeIcon icon={faEnvelope} size="3x" color='white' />
 
 function Navbar (){
 
@@ -8,9 +14,9 @@ function Navbar (){
             <h1>
             Fun Seafood Recipes
             </h1>
-            <a style={{margin:'10px', color:'marron'}}>Home</a>
-            <a style={{margin:'10px', color:'marron'}}>Order</a>
-            <a style={{margin:'10px', color:'marron'}}>Contact Us</a>
+            <a href='https://darckode.com/' style={{margin:'10px', color:'marron'}}>Home</a>
+            <a href='https://github.com/Josh-Darling13' style={{margin:'10px', color:'marron'}}>Back to GitHub</a>
+            <a href='mailto:squiddarling@gmail.com'>{emailIcon}</a>
         </div>
     )
 
